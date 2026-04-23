2afa71db32096aa7160a19354b902b8ca160f3fb
Anton <ostapov.anton2005@gmail.com>
2026-04-23 22:05:22 +0300

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,153 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { error } from 'node:console';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const users: any[] = [];
+
+function createGrid(bombsCount: number) {
+    const grid = new Array(25).fill(0);
+
+    let planted = 0;
+    while(planted < bombsCount)
+    {
+        const randomIndex = Math.floor(Math.random() * 25);
+        
+        if(!grid[randomIndex]) {
+            grid[randomIndex] = 1;
+            planted++;
+        }
+    }
+
+    return grid;
+}
+
+app.post('/register', (req: Request, res: Response) => {
+    const { username } = req.body;
+
+    const newPlayer = {
+        id: users.length + 1,
+        username: username,
+        balance: 1000
+    };
+
+    users.push(newPlayer);
+
+    console.log(`\n🎉 New Player: ${newPlayer.username}. Balance: ${newPlayer.balance} 💰\n`)
+
+    res.json(newPlayer);
+});
+
+
+app.post('/start-game', (req: Request, res: Response) => {
+    const {username, bet} = req.body;
+
+    const player = users.find(u => u.username === username);
+
+    if (!player)
+        return res.status(404).json({error: "Гравця не знайдено! Спочатку пройдіть реєстрацію."});
+
+    if (player.balance < bet)
+        return res.status(400).json({error: "Недостатньо грошей на балансі!"});
+
+    if(player.currentGame)
+        return res.status(400).json({error: "Ви вже у грі! Зробіть хід або заберіть куш."})
+
+    const grid = createGrid(3);
+    player.currentGame = grid;
+    player.currentWinnings = 0;
+
+    player.balance -= bet;
+
+    console.log(`\n🎲 Гравець ${player.username} зробив ставку ${bet}. Залишок: ${player.balance}\n`);
+
+    res.json({
+        message: "Ставка прийнята! Гра почалась.",
+        bet: bet,
+        currentBalance: player.balance
+    });
+});
+
+app.post('/click', (req: Request, res: Response) => {
+    const {username, cellIndex} = req.body;
+    
+    const player = users.find(u => u.username === username);
+
+    if(!player.currentGame)
+        return res.status(400).json({error: "Ви ще не зробили ставку та не розпочали гру!"});
+
+    const state = player.currentGame[cellIndex];
+
+    if(state === 1)
+    {
+        player.currentGame = null;
+        player.currentWinnings = 0;
+        return res.json({
+            message: "БУУУМ! Ви підірвалися.",
+            currentBalance: player.balance
+        })
+    };
+
+    if(state === 0)
+    {
+        player.currentWinnings += 50;
+        player.currentGame[cellIndex] = -1;
+        return res.json({
+            message: "СЕЙВ. +50 ПОТУЖНИХ",
+            currentWinnings: player.currentWinnings,
+            cellIndex: cellIndex
+        });
+    }
+    
+    return res.json({
+        message: "Ви вже відкрили цю комірку",
+        currentBalance: player.balance,
+        cellIndex: cellIndex
+    });
+});
+
+app.post('/cashout', (req: Request, res: Response) => {
+    const {username} = req.body;
+    const player = users.find(u => u.username === username);
+
+    if(!player.currentGame)
+        return res.status(400).json({error: "Гру не розпочато!"});
+
+    player.balance += player.currentWinnings;
+
+    player.currentGame = null;
+    player.currentWinnings = 0;
+
+    res.json({
+        message: "Виграш забрано!",
+        currentBalance: player.balance
+    })
+});
+
+app.post('/promo', (req: Request, res: Response) => {
+    const {username, code} = req.body;
+
+    const player = users.find(u => u.username === username);
+
+    if (!player)
+        return res.status(404).json({error: "Гравця не знайдено! Спочатку пройдіть реєстрацію."});
+
+    if(code == "BOOST2026")
+        player.balance += 500;
+    else
+        return res.status(400).json({error: "Невірний промокод"});
+
+    console.log(`🎁 Гравець ${player.username} успішно використав промокод BOOST! Поточний баланс: ${player.balance}`);
+
+    res.json({
+        message: "Промокод активований. Кошти нараховані",
+        currentBalance: player.balance
+    });
+});
+
+app.listen(3000, () => {
+    console.log('🚀 Сервер Сапера успішно запущено на http://localhost:3000');
+})
---
+++ frontend/src/App.tsx
@@ -0,0 +1,132 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [playerData, setPlayerData] = useState<any>();
+  const [grid, setGrid] = useState<any>({});
+  const [inGame, setInGame] = useState(false);
+  const API_URL = "http://localhost:3000";
+
+  const handleRegister = async () => {
+    if (!username) return alert('Йой, введіть ім\'я!');
+
+    const response = await fetch(`${API_URL}/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: username }),
+    });
+
+    const data = await response.json();
+    console.log("Привіт від сервера:", data);
+
+    setPlayerData(data);
+  }
+
+  const handleStartGame = async () => {
+    const response = await fetch(`${API_URL}/start-game`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: username, bet: 100 }),
+    });
+
+    const data = await response.json();
+    setPlayerData({ ...playerData, balance: data.currentBalance });
+    setGrid({});
+    setInGame(true);
+  }
+
+  const handleCellClick = async (index: number) => {
+    const response = await fetch(`${API_URL}/click`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: username, cellIndex: index }),
+    });
+
+    const data = await response.json();
+    if (data.error) {
+      return alert(data.error)
+    }
+
+    if (data.message === "СЕЙВ. +50 ПОТУЖНИХ") {
+      setGrid({ ...grid, [index]: 'safe' });
+      setPlayerData({ ...playerData, balance: data.currentBalance });
+    }
+
+    if (data.message === "БУУУМ! Ви підірвалися.") {
+      setGrid({ ...grid, [index]: 'bomb' });
+      setPlayerData({ ...playerData, balance: data.currentBalance });
+      setInGame(false);
+    }
+  }
+
+  const handleCashout = async () => {
+    const response = await fetch(`${API_URL}/cashout`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: username }),
+    });
+
+    const data = await response.json();
+    setPlayerData({ ...playerData, balance: data.currentBalance });
+    setGrid({});
+    setInGame(false);
+  }
+
+  return (
+    <div className="casino-wrapper">
+      <div className="glass-dashboard">
+        <h1 className="cyber-title">💣 Сапер 2077</h1>
+
+        {!playerData ? (
+          <div>
+            <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Вхід до Клубу</h2>
+            <div style={{ display: 'flex', gap: '15px', flexWrap: 'wrap', justifyContent: 'center' }}>
+              <input type="text" className="cyber-input" placeholder="Кодова кличка..." value={username} onChange={(e) => setUsername(e.target.value)} />
+              <button onClick={handleRegister} className="cyber-btn">Лудоманіяленд</button>
+            </div>
+          </div>
+        ) : (
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <h2>Welcome back, <span style={{ color: '#00ffff' }}>{playerData.username}</span></h2>
+            <p className="balance-text">Баланс: <span className="gold">{playerData.balance} 💰</span></p>
+
+            {!inGame && (<button className="cyber-btn" onClick={handleStartGame}>Депнуть хату</button>)}
+
+            <div className="mine-grid">
+              {Array.from({ length: 25 }).map((_, index) => {
+                // Визначаємо стиль для кожної клітинки
+                let extraClass = "";
+                if (grid[index] === 'safe') extraClass = "cell-safe";
+                if (grid[index] === 'bomb') extraClass = "cell-bomb";
+
+                return (
+                  <button
+                    key={index}
+                    className={`mine-cell ${extraClass}`}
+                    onClick={() => handleCellClick(index)}
+                  >
+                    {grid[index] === 'safe' ? '💎' : ''}
+                    {grid[index] === 'bomb' ? '💣' : ''}
+                  </button>
+                );
+              })}
+            </div>
+
+            {inGame && (<button className="cyber-btn cashout" onClick={handleCashout}>Забрати куш на жигулі</button>)}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
